09435dbe676ca20d3c017a381f7305e48c124282
Paul Nguon <herropaul@gmail.com>
2022-08-09 09:52:58 -0700

    finished designing login component

--- components/Login.js
+++ components/Login.js
@@ -1,10 +1,24 @@
 import React from 'react'
 import Head from "next/head";
+import { ChatIcon } from '@chakra-ui/icons';
+import { Box, Button, Center, Stack } from '@chakra-ui/react';
 
 export default function Login() {
   return (
-    <Head>
-        <title>Login</title>
-    </Head>
+    <>
+        <Head>
+            <title>Login</title>
+        </Head>
+
+        <Center height="100vh">
+          <Stack align="center" backgroundColor="gray.600" padding={20} rounded="3xl" spacing={10} boxShadow="lg">
+            <Box backgroundColor="blue.300" w="fit-content" padding={5} rounded="3xl" boxShadow="md">
+              <ChatIcon w="100px" h="100px" color="white" />
+            </Box>
+
+            <Button boxShadow="md">Sign in with google</Button>
+          </Stack>
+        </Center> 
+    </>
   )
 }
