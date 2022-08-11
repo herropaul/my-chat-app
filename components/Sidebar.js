aadb9f8d8bb3b6b35888af2795210ccc4235206e
Paul Nguon <herropaul@gmail.com>
2022-08-11 09:19:26 -0700

    chatlist is now dynamic, grabbing from our firestore db

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -4,32 +4,36 @@ import { signOut } from 'firebase/auth'
 import React from 'react'
 import { auth } from '../firebaseconfig'
 import { useAuthState } from 'react-firebase-hooks/auth';
-
-const Chat = () => {
-  return(
-    <Flex _hover={{bg: "gray.100", cursor: "pointer"}} padding={3} align="center">
-        <Avatar src="" marginRight={4}/>
-        <Text>User 1</Text>
-    </Flex>
-  )
-}
+import { useCollection } from 'react-firebase-hooks/firestore';
+import { collection } from 'firebase/firestore'
+import { db } from '../firebaseconfig'
 
 export default function Sidebar() {
   const [user] = useAuthState(auth);
+  const [snapshot, loading, error] = useCollection(collection(db, "chats"));
+  const chats = snapshot?.docs.map(doc => ({id: doc.id, ...doc.data()}));
+
+  const ChatList = () => {
+    return(
+      chats?.map(chat => 
+      <Flex key={Math.random()} _hover={{bg: "gray.100", cursor: "pointer"}} padding={3} align="center">
+        <Avatar src="" marginRight={4}/>
+        <Text>{chat.users}</Text>
+      </Flex>)
+    )
+  }
   
   return (
     /*
         Sidbar Div
     */
     <Flex 
-     //bg="blue.100"
      w="18rem" h="100vh" 
      borderEnd="1px solid" borderColor="gray.200"
      direction="column"
     >
         {/* Profile Div */}
         <Flex 
-         //bg="red.100" 
          w="100%" h="5rem" 
          align="center" justifyContent="space-between" 
          borderBottom="1px solid" borderColor="gray.200"
@@ -46,17 +50,7 @@ export default function Sidebar() {
         <Button m={5} p={6}>New Chat</Button>
 
         <Flex overflowX="scroll" direction="column" sx={{scrollbarWidth: "none"}} flex={1}>
-          <Chat/>
-          <Chat/>
-          <Chat/>
-          <Chat/>
-          <Chat/>
-          <Chat/>
-          <Chat/>
-          <Chat/>
-          <Chat/>
-          <Chat/>
-          <Chat/>
+          <ChatList/>
         </Flex>
 
     </Flex>
