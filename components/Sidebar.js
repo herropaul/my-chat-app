63952d8d77a69d984b5c736de57bab6381a1450e
Paul Nguon <herropaul@gmail.com>
2022-08-11 11:01:05 -0700

    filtered chats for current user and can redirect chat to its own route

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -7,18 +7,26 @@ import { useAuthState } from 'react-firebase-hooks/auth';
 import { useCollection } from 'react-firebase-hooks/firestore';
 import { collection } from 'firebase/firestore'
 import { db } from '../firebaseconfig'
+import getOtherEmail from '../utils/getOtherEmail'
+import { useRouter } from 'next/router'
 
 export default function Sidebar() {
   const [user] = useAuthState(auth);
   const [snapshot, loading, error] = useCollection(collection(db, "chats"));
   const chats = snapshot?.docs.map(doc => ({id: doc.id, ...doc.data()}));
+  const router = useRouter();
+
+  const redirect = (id) => {
+    router.push(`/chat/${id}`);
+  }
 
   const ChatList = () => {
     return(
-      chats?.map(chat => 
-      <Flex key={Math.random()} _hover={{bg: "gray.100", cursor: "pointer"}} padding={3} align="center">
+      chats?.filter(chat => chat.users.includes(user.email))
+      .map(chat => 
+      <Flex key={chat.id} _hover={{bg: "gray.100", cursor: "pointer"}} padding={3} align="center" onClick={() => redirect(chat.id)}>
         <Avatar src="" marginRight={4}/>
-        <Text>{chat.users}</Text>
+        <Text>{getOtherEmail(chat.users, user)}</Text>
       </Flex>)
     )
   }
